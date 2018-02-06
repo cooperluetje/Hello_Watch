f502b6428d96ec0e1c20fb5e424891271dcff646
Cooper Luetje <cooper@bytelion.com>
2018-02-05 22:38:06 -0500

    Initial solution commit

---
+++ Hello_Android_Wear/Hello_Apple_Watch/Hello_Apple_Watch.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace Hello_Apple_Watch.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
